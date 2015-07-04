38be27830b5b4fff2b5ce5a931f6dc53e7373347
John Berry <jarcane@gmail.com>
2015-07-04 22:48:58 +0300

    nums or no nums

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
+// wf - a word frequency counter
+// Copyright (c) 2015 John Berry
+// This program is licensed under the Affero GPL v3 License.
+// See agpl-3.0.txt for more information.
+
 use std::collections::HashMap;
 
-pub fn word_count(s: &str) -> HashMap<String, u32> {
-    s.split(|c: char| !c.is_alphanumeric())
-        .filter( |s| !s.is_empty() )
-        .map(|s| { s.chars().flat_map(char::to_lowercase).collect::<String>() })
-        .fold(HashMap::new(), |mut m, i| {
-            *m.entry(i).or_insert(0u32) += 1;
-            m
-        })
+fn word_freq<F>(s: &str, sf: F) -> HashMap<String, u32>
+    where F : Fn(char) -> bool {
+
+        s.split(sf)
+            .filter( |s| !s.is_empty() )
+            .map(|s| { s.chars().flat_map(char::to_lowercase).collect::<String>() })
+            .fold(HashMap::new(), |mut m, i| {
+                *m.entry(i).or_insert(0u32) += 1;
+                m
+            })
+    }
+
+fn word_freq_nums(s: &str) -> HashMap<String, u32> {
+    word_freq(s, |c: char| !c.is_alphanumeric())
+}
+
+fn word_freq_no_nums(s: &str) -> HashMap<String, u32> {
+    word_freq(s, |c: char| !c.is_alphabetic())
 }
 
 fn main() {
-    println!("{:?}", word_count("Dave is fat. \n Dave sucks. \n My name is Dave."));
+    let sample = "Dave is fat. \n Dave sucks. \n My name is Dave. 42.";
+    println!("{:?}", word_freq_no_nums(sample));
+    println!("{:?}", word_freq_nums(sample))
 }
