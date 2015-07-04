a923d30e1aaa84fe5b42c97be8224f6198630f91
John Berry <jarcane@gmail.com>
2015-07-04 23:42:25 +0300

    Sorting alphabetically

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@
 
 use std::collections::HashMap;
 
-fn word_freq<F>(s: &str, sf: F) -> HashMap<String, u32>
+type CountTable = HashMap<String, u32>;
+type OutputVec = Vec<String>;
+
+// The frequency counters
+fn word_freq<F>(s: &str, sf: F) -> CountTable
     where F : Fn(char) -> bool {
 
         s.split(sf)
@@ -17,16 +21,34 @@ fn word_freq<F>(s: &str, sf: F) -> HashMap<String, u32>
             })
     }
 
-fn word_freq_nums(s: &str) -> HashMap<String, u32> {
+fn word_freq_nums(s: &str) -> CountTable {
     word_freq(s, |c: char| !c.is_alphanumeric())
 }
 
-fn word_freq_no_nums(s: &str) -> HashMap<String, u32> {
+fn word_freq_no_nums(s: &str) -> CountTable {
     word_freq(s, |c: char| !c.is_alphabetic())
+}
+
+// Preparing and formatting output tables
+fn collect_table(c: CountTable) -> OutputVec {
+    let r: OutputVec = c.iter().map(|(k, v)| format!("{}, {}", k, v)).collect();
+    r
+}
+
+// Sorting
+fn sort_by_freq(c: CountTable) -> String {
+    "".to_string()
+} 
+
+fn sort_by_alpha(c: CountTable) -> String {
+    let mut arr = collect_table(c);
+    arr.sort();
+    arr.iter().fold("".to_string(), |i, v| i + v + "\n")
 }
 
 fn main() {
     let sample = "Dave is fat. \n Dave sucks. \n My name is Dave. 42.";
     println!("{:?}", word_freq_no_nums(sample));
-    println!("{:?}", word_freq_nums(sample))
+    println!("{:?}", word_freq_nums(sample));
+    print!("{}", sort_by_alpha(word_freq_no_nums(sample)));
 }
