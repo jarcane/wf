021301b8527e45b6ec3c54d95b4135014ad737a4
Annaia Berry <jarcane@gmail.com>
2018-09-20 12:45:09 +0300

    Optimize string joins in sort funcs

--- src/freq.rs
+++ src/freq.rs
@@ -1,28 +1,31 @@
 // Frequency analyzer
 
-use std::io::{StdinLock, BufRead};
-use std::collections::HashMap;
 use itertools::Itertools;
+use std::collections::HashMap;
+use std::io::{BufRead, StdinLock};
 
 pub enum Sorted {
     No,
     Alpha,
-    Freq
+    Freq,
 }
 
 // The frequency counters
 fn word_freq<F>(lines: &mut StdinLock, sf: F) -> HashMap<String, u32>
-    where F : Fn(char) -> bool {
-
+where
+    F: Fn(char) -> bool,
+{
     let mut dictionary = HashMap::new();
     let mut line = String::new();
+    let mut count = 0;
 
     while lines.read_line(&mut line).unwrap() > 0 {
         line.split(&sf)
-            .filter( |s| !s.is_empty() )
-            .map(|s| { s.to_string() })
-            .for_each(|s| *dictionary.entry(s).or_insert(0u32) += 1 );
-        
+            .filter(|s| !s.is_empty())
+            .map(|s| s.to_string())
+            .for_each(|s| *dictionary.entry(s).or_insert(0u32) += 1);
+        count += 1;
+        print!("curr line: {}\n", count);
         line.clear();
     }
 
@@ -39,32 +42,36 @@ fn word_freq_no_nums(s: &mut StdinLock) -> HashMap<String, u32> {
 
 // Preparing for output
 fn sort_by_freq(c: HashMap<String, u32>) -> String {
-    c.iter()
+    let counts: Vec<String> = c
+        .iter()
         .sorted_by(|a, b| Ord::cmp(&b.1, &a.1))
         .iter()
         .map(|&(k, v)| format!("{} {}", k, v))
-        .fold("".to_string(), |i, v| format!("{}{}\n", i, v))
-} 
+        .collect();
+
+    counts.join("\n")
+}
 
 fn sort_by_alpha(c: HashMap<String, u32>) -> String {
     let mut arr: Vec<String> = c.iter().map(|(k, v)| format!("{} {}", k, v)).collect();
     arr.sort();
-    arr.iter().fold("".to_string(), |i, v| i + v + "\n")
+    arr.join("\n")
 }
 
 fn no_sort(c: HashMap<String, u32>) -> String {
-    c.iter().map(|(k, v)| format!("{} {}", k, v)).fold("".to_string(), |i, v| format!("{}{}\n", i, v))
+    let counts: Vec<String> = c.iter().map(|(k, v)| format!("{} {}", k, v)).collect();
+    counts.join("\n")
 }
 
 // The main dispatch function
 pub fn get_freqs(s: &mut StdinLock, nums: bool, sort: Sorted) -> String {
     let count = match nums {
-        true  => word_freq_nums(s),
+        true => word_freq_nums(s),
         false => word_freq_no_nums(s),
     };
     match sort {
-        Sorted::No    => no_sort(count),
+        Sorted::No => no_sort(count),
         Sorted::Alpha => sort_by_alpha(count),
-        Sorted::Freq  => sort_by_freq(count),
+        Sorted::Freq => sort_by_freq(count),
     }
 }
